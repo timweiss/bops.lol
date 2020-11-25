28729222a0291eeeaadaf1a1ff9b0a4f3ccef3a3
Tim W <tim@weiss.moe>
2020-11-25 20:33:16 +0100

    better OpenGraph

--- src/App.svelte
+++ src/App.svelte
@@ -5,21 +5,6 @@
 	let lists: Playlist[] = playlists;
 </script>
 
-<svelte:head>
-	<title>bops.lol - Explore Good Spotify Playlists</title>
-	<meta name="description" content="Explore good Spotify playlists on bops.lol, where users can add their own playlists for you to enjoy!" />
-	<meta name="keywords" content="Spotify, Playlist, Music, bop" />
-	<meta name="author" content="Tim Weiß" />
-	<meta property="og:title" content="Explore Good Spotify Playlists"/> 
-	<meta property="og:description" content="Explore good Spotify playlists on bops.lol, where users can add their own playlists for you to enjoy!" /> 
-	<meta property="og:image" content="https://bops.lol/og.png" /> 
-	<meta property="og:url" content="https://bops.lol" />
-	<meta property="twitter:creator" content="@supplyanddem" />
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta charset="utf-8"/>
-	<html lang="en" />
-</svelte:head>
-
 <main>
 	<h1>Explore Good Spotify Playlists</h1>
 	<p>This is a highly opinionated list of playlists that you are free to extend with your own additions!</p>
